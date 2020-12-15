3520b59c53adc60a435d5f651148c6d300504b99
Simone Cardinale <simonecardinale8@gmail.com>
2020-12-15 17:19:30 +0100

    Style + js complete except ticket price

---
+++ js/script.js
@@ -0,0 +1,48 @@
+var nome = document.getElementById('nome').value;
+console.log(nome);
+var chilometri = document.getElementById('chilometri').value;
+console.log(chilometri);
+var eta = document.getElementById('eta').value;
+console.log(eta);
+
+
+// calcolo tariffa e caratteristiche biglietto
+var prezzo = chilometri*0.21
+var offerta = "Tariffa standard"
+
+// tariffa
+if (eta == 'minorenne'){
+    prezzo-=prezzo*20/100;
+    offerta = "Sconto minorenni";
+} else if (eta == 'over65') {
+    prezzo-=prezzo*40/100;
+    offerta = "Sconto Over 65"
+}
+
+
+//buttons
+
+
+
+// stampe
+var show = document.getElementById('genera');show.addEventListener('click', function() {
+    document.getElementById('nomePasseggero').innerHTML = nome;
+    document.getElementById('offerta_applicata').innerHTML = offerta;
+    document.getElementById("costoBiglietto").innerHTML += prezzo.toFixed(2) + " €";
+    document.getElementById('numeroCarrozza').innerHTML = Math.floor(Math.random()*9);
+    document.getElementById('codiceBiglietto').innerHTML = Math.floor(Math.random()*(100000-90000)+90000);
+    }
+);
+
+var hidden = document.getElementById('annulla');hidden.addEventListener('click', function() {
+    document.getElementById('nomePasseggero').innerHTML = "";
+    document.getElementById('offerta_applicata').innerHTML = "";
+    document.getElementById("costoBiglietto").innerHTML = "";
+    document.getElementById('numeroCarrozza').innerHTML = "";
+    document.getElementById('codiceBiglietto').innerHTML = "";
+    }
+);
+
+
+
+
